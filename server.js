06a1cdda0f14861be39dd66483b30be8d01c86b1
ratriczek <68536474+ratriczek@users.noreply.github.com>
2020-07-21 13:26:12 +0200

    zadanie 2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 //
 const clientId = process.env.CLIENT_ID || "C4d2626189e40ffbde5f8d2948650bda5b4261804986bb3a977b079d2af2d7d93";
 const clientSecret = process.env.CLIENT_SECRET || "81772d83ee75a5835d2b19a1c9e95b47bf6618a3a736e361c5324dc18e7183e8";
-const scopes = process.env.SCOPES || "spark:people_read"; // supported scopes are documented at: https://developer.webex.com/add-integration.html, the scopes separator is a space, example: "spark:people_read spark:rooms_read"
+const scopes = process.env.SCOPES || "spark:rooms_read"; // supported scopes are documented at: https://developer.webex.com/add-integration.html, the scopes separator is a space, example: "spark:people_read spark:rooms_read"
 
 // Compute redirect URI where your integration is waiting for Webex cloud to redirect and send the authorization code
 // unless provided via the REDIRECT_URI variable
@@ -251,7 +251,7 @@ function oauthFlowCompleted(access_token, res) {
       // Uncomment to send feedback via static HTML code 
       //res.send("<h1>OAuth Integration example for Webex (static HTML)</h1><p>So happy to meet, " + json.displayName + " !</p>");
       // Current code leverages an EJS template:
-      const str = read(join(__dirname, '/www/display-name.ejs'), 'utf8');
+      const str = read(join(__dirname, '/togofurther/list-rooms.ejs'), 'utf8');
       const compiled = ejs.compile(str)({ "displayName": json.displayName });
       res.send(compiled);
    });
